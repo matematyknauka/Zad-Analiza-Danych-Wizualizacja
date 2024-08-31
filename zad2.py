b11d4fcf2d78bf2f11335f8fa4bae1009f9b9c80
matematyknauka <matematyknauka@gmail.com>
2024-08-31 19:55:09 +0000

    Modyfikacja pliku zad2.py - osobna tabela w pliku dla każdego produktu

--- zad2.py
+++ zad2.py
@@ -1,6 +1,14 @@
 import pandas as pd
 import zad2ModulFiltrowanieSelect as flt
+import shutil as sh
+import os
+
+directory = './tab_prod_csv'
+if os.path.exists(directory):
+  sh.rmtree(directory)
+os.makedirs(directory)
+
 df_csv = pd.read_csv('tabela.csv')['Produkt'].unique() # Wypisuje raz każdą wartośś z kolumny.
 
 for produkt in df_csv:
-    print(flt.filtr('tabela.csv', 'Produkt', produkt)) # Można do pliku w nowym katalogu zapisać.
+    flt.filtr('tabela.csv', 'Produkt', produkt).to_csv(directory + '/' + produkt + '.csv')

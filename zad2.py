74b7fe6b46fae1c10884f8382a0b9c7cf3efa2fa
matematyknauka <matematyknauka@gmail.com>
2024-08-31 20:26:37 +0000

    Modyfikacja Do_zrobienia.txt oraz zad2.py

--- zad2.py
+++ zad2.py
@@ -3,12 +3,12 @@ import zad2ModulFiltrowanieSelect as flt
 import shutil as sh
 import os
 
-directory = './tab_prod_csv'
-if os.path.exists(directory):
-  sh.rmtree(directory)
-os.makedirs(directory)
+directory = './tab_prod_csv' # Kropka oznacza obecny katalog roboczy.
+if os.path.exists(directory): # Sprawdzenie, czy katalog directory istnieje
+  sh.rmtree(directory) # Jeśli istnieje to go usuwa.
+os.makedirs(directory) # Tworzy pusty directory.
 
-df_csv = pd.read_csv('tabela.csv')['Produkt'].unique() # Wypisuje raz każdą wartośś z kolumny.
+df_csv = pd.read_csv('tabela.csv')['Produkt'].unique() # Wypisuje raz każdą wartość z kolumny Produkt.
 
-for produkt in df_csv:
-    flt.filtr('tabela.csv', 'Produkt', produkt).to_csv(directory + '/' + produkt + '.csv')
+for produkt in df_csv: # Iteruje po produktach. Ewentualnie ich ID.
+    flt.filtr('tabela.csv', 'Produkt', produkt).to_csv(directory + '/' + produkt + '.csv') # Dla każdego produktu robi tabelę. Czyli coś jak Select...
